9248507ac550e870887bcda5e2f6499ce8f1d2ae
Benjamin Komen <komenb@gmail.com>
2019-11-08 21:50:57 +0100

    Starting with main entry point.

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+var router = mux.NewRouter()
+
+func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         fmt.Sprintf("127.0.0.1:%s", port),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
